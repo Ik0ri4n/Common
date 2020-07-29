618fe3d5893b9499b1a8668469bc7f4fce04bf6d
Dominik <dv39@waibel-hh.de>
2020-07-29 14:32:30 +0200

    Add abstract ServiceBase
    Add a service base type that can be extended to create services for
    dependency injection.

---
+++ src/ServiceBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// Represents a service that can be used with dependency injection.
+    /// </summary>
+    public abstract class ServiceBase : IAsyncInitialisable, IDisposable, IAsyncDisposable
+    {
+        private bool initialised;
+        private bool disposed;
+
+        /// <inheritdoc/>
+        public async ValueTask InitialiseAsync()
+        {
+            if (!initialised && !disposed)
+            {
+                initialised = true;
+                await InitialiseServiceAsync();
+            }
+        }
+
+        protected virtual ValueTask InitialiseServiceAsync()
+            => new ValueTask();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        protected virtual void Dispose(bool disposing)
+#pragma warning restore IDE0060 // Remove unused parameter
+            => disposed = true;
+
+        /// <inheritdoc/>
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeServiceAsync();
+
+            Dispose(false);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual ValueTask DisposeServiceAsync()
+            => default;
+
+        protected void CheckValidState()
+        {
+            if (!initialised)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (disposed)
+            {
+                throw new ObjectDisposedException(GetType().FullName);
+            }
+        }
+    }
+}

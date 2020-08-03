85c1a5708c8a7def4c271f9ad2a2d889cec2cf59
Dominik <dv39@waibel-hh.de>
2020-08-03 14:25:58 +0200

    Complete ServiceBase documentation
    Add missing documentation for protected members of the ServiceBase
    class.

--- src/Common/ServiceBase.cs
+++ src/Common/ServiceBase.cs
@@ -21,6 +21,14 @@ namespace Common
             }
         }
 
+        /// <summary>
+        /// Initialises service-specific ressources asynchronously.
+        /// </summary>
+        /// <remarks>
+        /// This method is called once during service initialisation with 
+        /// <see cref="InitialiseAsync"/>.
+        /// </remarks>
+        /// <returns>A task that represents the asynchronous initialise operation.</returns>
         protected virtual ValueTask InitialiseServiceAsync()
             => new ValueTask();
 
@@ -31,6 +39,11 @@ namespace Common
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposes service-specific unmanaged and, if called deterministically, managed ressources synchronously.
+        /// </summary>
+        /// <remarks>Subsequent calls to this method should do nothing.</remarks>
+        /// <param name="disposing"><see langword="true"/> if called deterministically.</param>
 #pragma warning disable IDE0060 // Remove unused parameter
         protected virtual void Dispose(bool disposing)
 #pragma warning restore IDE0060 // Remove unused parameter
@@ -45,9 +58,19 @@ namespace Common
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposes service-specific managed ressources asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous dispose operation.</returns>
         protected virtual ValueTask DisposeServiceAsync()
             => default;
 
+        /// <summary>
+        /// Checks wether the service instance is in a valid state to 
+        /// allow access to its members.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The service is not initialised.</exception>
+        /// <exception cref="ObjectDisposedException">The service is already disposed.</exception>
         protected void CheckValidState()
         {
             if (!initialised)
